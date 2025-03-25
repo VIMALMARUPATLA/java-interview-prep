5b0554e27130a5d702f3c44d8940ff3486637640
VIMALMARUPATLA <48025945+VIMALMARUPATLA@users.noreply.github.com>
2025-03-25 20:33:27 +0530

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(reg => console.log('Service Worker registered'))
+            .catch(err => console.log('Service Worker registration failed:', err));
+    });
+}
+// ... (rest of the JavaScript from index.html)
---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'java-interview-prep-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    'https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/prism.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/themes/prism-okaidia.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
